50a5e53d496b9ad84245353c9f2d35b4cfc6c883
TigerTJJ <t1g3rtjj@gmail.com>
2022-08-07 23:30:10 -0400

    manifest v3 related
    wake up babe, new way to inject js code outside of the protected chrome extension sandbox dropped!

--- learn-inject1.js
+++ learn-inject1.js
@@ -1,8 +1,20 @@
-function injectScript(file_path, tag) {
-    var node = document.getElementsByTagName(tag)[0];
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', file_path);
-    node.appendChild(script);
-  }
-  injectScript(chrome.extension.getURL('learn-inject2.js'), 'body');
+// function injectScript(file_path, tag) {
+//     var node = document.getElementsByTagName(tag)[0];
+//     var script = document.createElement('script');
+//     script.setAttribute('type', 'text/javascript');
+//     script.setAttribute('src', file_path);
+//     node.appendChild(script);
+//   }
+//   injectScript(chrome.extension.getURL('learn-inject2.js'), 'body');
+
+// chrome.scripting.executeScript({
+//   target: { tabId: tab.id },
+//   files: ['learn-inject2.js']
+// });
+
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('learn-inject2.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
